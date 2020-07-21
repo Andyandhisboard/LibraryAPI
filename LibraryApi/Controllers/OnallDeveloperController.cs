bceffeb21d320ec1561dab259476fe4d306d3b01
Andyandhisboard <67791240+Andyandhisboard@users.noreply.github.com>
2020-07-21 15:50:41 -0600

    Docker stuff

---
+++ LibraryApi/Controllers/OnallDeveloperController.cs
@@ -0,0 +1,27 @@
+﻿using LibraryApi.Models;
+using LibraryApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Controllers
+{
+    public class OnallDeveloperController : ControllerBase
+    {
+        ILookupOnCallDevelopers OnCallLookup;
+
+        public OnallDeveloperController(ILookupOnCallDevelopers onCallLookup)
+        {
+            OnCallLookup = onCallLookup;
+        }
+
+        [HttpGet("/oncalldeveloper")]
+        public async Task<ActionResult> GetOnCallDeveloper()
+        {
+            OnCallDeveloperResponse response = await OnCallLookup.GetDeveloper();
+            return Ok(response);
+        }
+    }
+}
